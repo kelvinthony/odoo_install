753a97f6519af4332ee38f9ea585a814486c6193
kmpragmatic <kmeza@pragmatic.com.pe>
2022-02-28 18:04:18 -0500

    Initial Commit

--- install.sh
+++ install.sh
@@ -19,15 +19,15 @@ WKHTMLTOX=https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wk
 # Actualizar los repositorios en Ubuntu
 #--------------------------------------------------
 echo -e "\n--- Actualizando servidor ---"
-sudo apt-get update
+#sudo apt-get update
 echo -e "\n--- Instalando Postgresql ---"
-sudo apt install postgresql -y
+#sudo apt install postgresql -y
 #--------------------------------------------------
 # Instar dependencias
 #--------------------------------------------------
 echo -e "\n--- Instalando wkhtmltopdf ---"
-wget $WKHTMLTOX
-sudo apt install ./wkhtmltox_0.12.6-1.focal_amd64.deb
+#wget $WKHTMLTOX
+#sudo apt install ./wkhtmltox_0.12.6-1.focal_amd64.deb
 sudo apt -f install -y
 echo -e "\n--- Instalando pip3 ---"
 sudo apt-get -y install python3-pip
@@ -39,31 +39,35 @@ echo -e "\n--- Instalando pip3 - pyOpenSSL ---"
 sudo pip3 install pyOpenSSL
 echo -e "\n--- Actualizando pip3 - reportlab ---"
 sudo pip3 install reportlab --upgrade
+echo -e "\n--- Actualizando pip3 - python3-testresources ---"
+sudo apt-get install -y python3-testresources
 echo -e "\n--- Install Odoo ---"
 #wget -O - https://nightly.odoo.com/odoo.key | apt-key add -
 #echo "deb http://nightly.odoo.com/${OE_VERSION}/nightly/deb/ ./" >> /etc/apt/sources.list.d/odoo.list
 apt-get update && apt-get install odoo -y
 if [ $IS_ENTERPRISE = "True" ]; then
-    sudo pip3 install psycopg2-binary pdfminer.six
-    echo -e "\n--- Create symlink for node"
-    sudo ln -s /usr/bin/nodejs /usr/bin/node
-    sudo su $OE_USER -c "mkdir mnt/enterprise"
-    sudo su $OE_USER -c "mkdir mnt/enterprise/addons"
+  sudo pip3 install psycopg2-binary pdfminer.six
+  echo -e "\n--- Create symlink for node"
+  sudo ln -s /usr/bin/nodejs /usr/bin/node
+  sudo su $OE_USER -c "mkdir /mnt/enterprise"
 
-    GITHUB_RESPONSE=$(sudo git clone --depth 1 --branch $OE_VERSION https://www.github.com/odoo/enterprise "mnt/enterprise" 2>&1)
-    while [[ $GITHUB_RESPONSE == *"Authentication"* ]]; do
-        echo "------------------------WARNING------------------------------"
-        echo "Your authentication with Github has failed! Please try again."
-        printf "In order to clone and install the Odoo enterprise version you \nneed to be an offical Odoo partner and you need access to\nhttp://github.com/odoo/enterprise.\n"
-        echo "TIP: Press ctrl+c to stop this script."
-        echo "-------------------------------------------------------------"
-        echo " "
-        GITHUB_RESPONSE=$(sudo git clone --depth 1 --branch $OE_VERSION https://www.github.com/odoo/enterprise "mnt/enterprise/addons" 2>&1)
-    done
+  GITHUB_RESPONSE=$(sudo git clone --depth 1 --branch $OE_VERSION https://www.github.com/odoo/enterprise "/mnt/enterprise" 2>&1)
+  while [[ $GITHUB_RESPONSE == *"Authentication"* ]]; do
+    echo "------------------------Atencion------------------------------"
+    echo "La autenticación con github ha fallado! por favor, intenta de nuevo."
+    printf "Para clonar e instalar la versión empresarial de Odoo, \n Necesita ser un socio oficial de Odoo y necesita acceso a\nhttp://github.com/odoo/enterprise.\n"
+    echo "TIP: Presiona ctrl+c para deterner el script."
+    echo "-------------------------------------------------------------"
+    echo " "
+    GITHUB_RESPONSE=$(sudo git clone --depth 1 --branch $OE_VERSION https://www.github.com/odoo/enterprise "/mnt/enterprise" 2>&1)
+  done
 
-    echo -e "\n---- Added Enterprise code under mnt/enterprise ----"
-    echo -e "\n---- Installing Enterprise specific libraries ----"
-    sudo -H pip3 install ofxparse dbfread ebaysdk firebase_admin
-    sudo npm install -g less
-    sudo npm install -g less-plugin-clean-css
+  echo -e "\n---- Added Enterprise code under mnt/enterprise ----"
+  echo -e "\n---- Installing Enterprise specific libraries ----"
+  sudo -H pip3 install ofxparse dbfread ebaysdk firebase_admin
+  sudo npm install -g less
+  sudo npm install -g less-plugin-clean-css
+fi
+if [ $IS_ENTERPRISE = "True" ]; then
+  sudo su root -c "printf 'addons_path=/mnt/enterprise,/usr/lib/python3/dist-packages/odoo/addons\n' >> /etc/odoo/odoo.conf"
 fi
